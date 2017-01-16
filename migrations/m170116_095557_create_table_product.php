fa6b856ad039462108ac48020d3f27ac2eb38000
antydemant <ostap4ukigor@i.ua>
2017-01-16 13:10:48 +0200

    update table product

--- migrations/m170116_095557_create_table_product.php
+++ migrations/m170116_095557_create_table_product.php
@@ -1,12 +1,24 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
+
 
 class m170116_095557_create_table_product extends Migration
 {
     public function up()
     {
-
+        $this->createTable('product', [
+            'id' => Schema::TYPE_PK,
+            'product_name' => Schema::TYPE_STRING . ' NOT NULL',
+            'description' => Schema::TYPE_TEXT,
+            'weight' => Schema::TYPE_DOUBLE,
+            'ingredients' => Schema::TYPE_TEXT,
+            'price' => Schema::TYPE_DOUBLE,
+            'photo_url' => Schema::TYPE_STRING . '(255) NOT NULL',
+            'date_add' => Schema::TYPE_DATE,
+            'serv_id' => Schema::TYPE_INTEGER,
+        ]);
     }
 
     public function down()
