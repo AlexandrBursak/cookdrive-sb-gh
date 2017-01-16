edc46807da02931eb82ad24c96baa5fd022e42ac
RomanKis <roman.kis.p@gmal.com>
2017-01-16 12:41:28 +0200

    orders

---
+++ migrations/m170116_103931_orders.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170116_103931_orders extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170116_103931_orders cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
