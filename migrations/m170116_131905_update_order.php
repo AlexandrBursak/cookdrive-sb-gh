d9fc870b8473992b7b29727b2183150335fc2ddc
antydemant <ostap4ukigor@i.ua>
2017-01-16 15:29:11 +0200

    update order table

--- migrations/m170116_131905_update_order.php
+++ migrations/m170116_131905_update_order.php
@@ -1,12 +1,23 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m170116_131905_update_order extends Migration
 {
     public function up()
     {
+        $this->dropTable('order');
 
+        $this->createTable('order', [
+            'id' => Schema::TYPE_PK,
+            'date' => Schema::TYPE_DATE,
+            'user_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'product_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'quantity' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'product_name' => Schema::TYPE_STRING . '(255) NOT NULL',
+            'price' => Schema::TYPE_DOUBLE . ' NOT NULL',
+        ]);
     }
 
     public function down()
