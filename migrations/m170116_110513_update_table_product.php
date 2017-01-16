ea766a10c0b05d24d850787425d73dfaf2b6a8b2
antydemant <ostap4ukigor@i.ua>
2017-01-16 13:11:01 +0200

    update table product

--- migrations/m170116_110513_update_table_product.php
+++ migrations/m170116_110513_update_table_product.php
@@ -1,12 +1,27 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m170116_110513_update_table_product extends Migration
 {
     public function up()
     {
+        $this->dropTable('product');
 
+        $this->createTable('product', [
+            'id' => Schema::TYPE_PK,
+            'product_name' => Schema::TYPE_STRING . ' NOT NULL',
+            'description' => Schema::TYPE_TEXT,
+            'weight' => Schema::TYPE_DOUBLE,
+            'ingredients' => Schema::TYPE_TEXT,
+            'price' => Schema::TYPE_DOUBLE,
+            'photo_url' => Schema::TYPE_STRING . '(255) NOT NULL',
+            'date_add' => Schema::TYPE_DATE,
+            'category' => Schema::TYPE_STRING . '(255)',
+            'sub_category' => Schema::TYPE_STRING . '(255)',
+            'serv_id' => Schema::TYPE_INTEGER,
+        ]);
     }
 
     public function down()
