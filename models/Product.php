376c27ca8420cf232462b8b8887e0147b113cf7c
Ira <irynanikol45@gmail.com>
2017-01-17 18:13:30 +0200

    Created Active Record classes for relations between Order User-Product-Service

---
+++ models/Product.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 17.01.17
+ * Time: 17:34
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Product extends ActiveRecord
+{
+
+}
---
+++ models/Service.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 17.01.17
+ * Time: 17:30
+ */
+
+namespace app\models;
+
+
+class Service extends ActiveRecord
+{
+
+}
