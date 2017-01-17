011afeef04bfc65e02b63bbca7550cb1b8aefcc7
antydemant <ostap4ukigor@i.ua>
2017-01-17 15:47:34 +0200

    update table order

---
+++ migrations/m170117_134350_add_column_serv_id.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170117_134350_add_column_serv_id extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170117_134350_add_column_serv_id cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
