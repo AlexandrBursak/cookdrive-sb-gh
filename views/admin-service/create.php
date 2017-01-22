220d01a1b78530526cd1074b35213f248b4ffdce
antydemant <ostap4ukigor@i.ua>
2017-01-22 18:08:47 +0200

    by CRUD generator for Admin role

---
+++ views/admin-service/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Service */
+
+$this->title = 'Create Service';
+$this->params['breadcrumbs'][] = ['label' => 'Services', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="service-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
