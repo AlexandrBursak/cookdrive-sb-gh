b4912ee4e01b0f62b29b744dc5076d0afd6da1cf
zlemore <irynanikol45@gmail.com>
2017-01-24 15:28:04 +0200

    db.php

--- models/Product.php
+++ models/Product.php
@@ -10,8 +10,12 @@ namespace app\models;
 
 
 use yii\db\ActiveRecord;
+use app\models\Service;
 
-class Product extends ActiveRecord
-{
+
+class Product extends ActiveRecord{
+    public function getService(){
+        return $this->hasOne(Service::className(), ['id' => 'serv_id']);
+    }
 
 }
