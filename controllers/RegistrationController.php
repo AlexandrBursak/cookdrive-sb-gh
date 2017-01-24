3f50366e3f2ed433f7e11986843972bddd28f281
antydemant <ostap4ukigor@i.ua>
2017-01-24 15:10:08 +0200

    added Profile data

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -7,9 +7,63 @@
  */
 
 namespace app\controllers;
+use dektrium\user\controllers\RegistrationController as BaseRegistrationController;
+use dektrium\user\models\Profile;
+use dektrium\user\models\User;
+use yii\web\NotFoundHttpException;
 
 
-class RegistrationController
+class RegistrationController extends  BaseRegistrationController
 {
+
+    public function actionConnect($code)
+    {
+        $account = $this->finder->findAccount()->byCode($code)->one();
+
+        if ($account === null || $account->getIsConnected()) {
+            throw new NotFoundHttpException();
+        }
+
+        /** @var User $user */
+        $user = \Yii::createObject([
+            'class'    => User::className(),
+            'scenario' => 'connect',
+            'username' => $account->username,
+            'email'    => $account->email,
+        ]);
+        
+        $event = $this->getConnectEvent($account, $user);
+
+        $this->trigger(self::EVENT_BEFORE_CONNECT, $event);
+
+        $account_json = json_decode($account->data, true);
+
+
+        $profile = \Yii::createObject([
+            'class' => Profile::className(),
+            'name' => $account_json['name']['familyName'] . ' ' . $account_json['name']['givenName'],
+            'public_email' => $account->email,
+            'gravatar_email' => $account->email,
+            'website' => $account_json['url'],
+            'photo_url' => substr($account_json['image']['url'],0, -6),
+        ]);
+
+        $user->profile = $profile;
+        
+        if ($user->load(\Yii::$app->request->post()) && $user->create()) {
+            
+            $account->connect($user);
+            $this->trigger(self::EVENT_AFTER_CONNECT, $event);
+
+            \Yii::$app->user->login($user, $this->module->rememberFor);
+
+            return $this->goBack();
+        }
+
+        return $this->render('connect', [
+            'model'   => $user,
+            'account' => $account,
+        ]);
+    }
 
 }
