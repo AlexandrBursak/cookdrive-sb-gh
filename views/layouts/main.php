8eeb57baf464fafb42705cfc11b21a2466ec07fb
antydemant <osap4ukigor@i.ua>
2017-01-30 18:21:32 +0200

    updated search form

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,13 +23,6 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
-<?php/*
-echo '<pre>';
-$param = Yii::$app->user->profile;
-var_dump($param);
-echo '</pre>';*/
-?>
-Yii::$app->user->identity
 <?php $this->beginBody() ?>
 
 <div class="wrap">
@@ -38,59 +31,84 @@ Yii::$app->user->identity
     $qty_head = $session['cart.qty'];
     $qty_head = !$qty_head ? 0 : $qty_head;
     NavBar::begin([
-        'brandLabel' => 'My food',
+        'brandLabel' => 'CookDrive for SoftBistro',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar-inverse navbar-top',
         ],
     ]);
+    $menuItems = [
+        ['label' => $qty_head . ' шт.', 'url' => ['/cart/index'], 'options' => ['class' => 'cart_navbar']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = '<li>' .Connect::widget([ 'baseAuthUrl' => ['/user/security/auth']]). '</li>';
+    } else {
+
+        $subitems = [];
+        if(Yii::$app->user->identity->username == 'admin')
+        {
+            $subitems[] = [
+                'label' => 'Користувачі',
+                'url' => ['/users'],
+            ];
+
+            $subitems[] = [
+                'label' => 'Замовлення',
+                'url' => ['/users'],
+            ];
+
+            $subitems[] = [
+                'label' => 'Мій кабінет',
+                'url' => ['/user/settings'],
+            ];
+
+            $subitems[] = [
+                'label' => 'Вихід',
+                'url' => ['/site/logout'],
+                'linkOptions' => ['data-method' => 'post'],
+            ];
+
+        } else
+        {
+            $subitems[] = [
+                'label' => 'Мій кабінет',
+                'url' => ['/user/settings'],
+            ];
+
+            $subitems[] = [
+                'label' => 'Вихід',
+                'url' => ['/site/logout'],
+                'linkOptions' => ['data-method' => 'post'],
+            ];
+
+        }
+
+        $menuItems[] = ['label' => Yii::$app->user->identity->profile->name,
+            'items' => $subitems,
+        ];
+
+    }
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            // ['label' => 'Home', 'url' => ['/site/index']],
-            // ['label' => 'About', 'url' => ['/site/about']],
-            // ['label' => 'Contact', 'url' => ['/site/contact']],
-            [
-                'label' => $qty_head . ' шт.', 
-                'url' => ['/cart/index'],
-                'options' => ['class' => 'cart_navbar']
-            ],
-            Yii::$app->user->isGuest ? (
-                '<li>' .Connect::widget([ 'baseAuthUrl' => ['/user/security/auth']]). '</li>') : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->profile->name . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-
-        ],
-
+        'options' => ['class' =>'navbar-nav navbar-right'],
+        'items' => $menuItems,
     ]);
     NavBar::end();
+
     ?>
 
 
 
     <div class="container">
-
-
-            <div class="row">
+        <div class="row">
             <div class="col-lg-12">
-                <div class="input-group">
-                    <form method="get" action="<?=\yii\helpers\Url::to(['category/search']) ?>">
+                <form method="get" action="<?=\yii\helpers\Url::to(['category/search']) ?>" class="input-group">
                     <input type="text" class="form-control" name="params" placeholder="Введіть дані для пошуку">
-                    </form>
                     <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Шукати</button>
-      </span>
-                </div><!-- /input-group -->
-            </div><!-- /.col-lg-6 -->
+                                <input class="btn btn-default" type="submit" value="Шукати">
+                            </span>
+                </form>
+            </div><!-- /.col-lg-12 -->
         </div>
-
 
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
