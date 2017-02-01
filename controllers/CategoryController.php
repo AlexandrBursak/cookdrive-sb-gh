c6130f34f81d29c0f1645fbb5664c52f72db9fae
zlemore <irynanikol45@gmail.com>
2017-02-01 11:27:13 +0200

    Added redirect into actionSearch

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -28,13 +28,16 @@ class CategoryController extends Controller {
 	}
 	public function actionSearch(){
         $params = Yii::$app->request->get('query');
-        $products=Product::find()->asArray()->where(['like', 'sub_category', $params])
-            ->orWhere(['like', 'product_name', $params])
-            ->all();
-        foreach ($products as $key => $value) {
-            $new_arr[$value['sub_category']][]=$value;
+        if (!$params=='') {
+            $products = Product::find()->asArray()->where(['like', 'sub_category', $params])
+                ->orWhere(['like', 'product_name', $params])
+                ->all();
+            foreach ($products as $key => $value) {
+                $new_arr[$value['sub_category']][] = $value;
+            }
+            return $this->render('view', compact('new_arr'));
         }
-        return $this->render('view', compact('new_arr'));
+        else $this->redirect(\yii\helpers\Url::to(['@web/index.php']));
 
     }
 }
