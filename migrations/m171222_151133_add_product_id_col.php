efd8004a1cb764fe52e3b68dcef8a04c0f90e6eb
Owi4k <only3143@gmail.com>
2017-12-22 17:19:26 +0200

    Migration: add Product_id column

---
+++ migrations/m171222_151133_add_product_id_col.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m171222_151133_add_product_id_col
+ */
+class m171222_151133_add_product_id_col extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('product', 'product_id', $this->string()->after('link'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m171222_151133_add_product_id_col cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171222_151133_add_product_id_col cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
