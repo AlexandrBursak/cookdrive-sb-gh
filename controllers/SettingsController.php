d01f95e3a74306da251dc377f35ee1ddfb5cefbb
yuzvyshyn <fakcelo@gmail.com>
2017-12-26 17:37:30 +0200

    Nothing added

--- controllers/SettingsController.php
+++ controllers/SettingsController.php
@@ -21,12 +21,9 @@ class SettingsController extends BaseSettingsController
     public function actionOrders() {
     	$authorized_user_id = Yii::$app->user->id;
     	$product_user = Order::find()->select('date, user_id, product_name, product_price, product_serv_id, SUM(quantity) AS quantity')->groupBy(['date', 'user_id','product_name', 'product_price', 'product_serv_id' ])->asArray()->where(['user_id' => $authorized_user_id])->orderBy(['date' => SORT_DESC])->all();
-
     	foreach ($product_user as $key => $value) {
 			$product_user_in_date[$value['date']][]=$value;
 		}
-        
-    	return $this->render('orders', compact('product_user_in_date'));
     }
 
 }
